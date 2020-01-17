660b722bea5a030ecf3e8479d9afc84c908b8386
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-17 18:59:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
  def self.find_or_create_by_name(name)
    if self.find_by_name(name)
       self.find_by_name(name)
-      #binding.pry
+      binding.pry
    else
       self.create(name)
    end
